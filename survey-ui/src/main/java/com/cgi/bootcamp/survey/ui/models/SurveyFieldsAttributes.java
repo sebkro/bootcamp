af4ccb765994bdfebf6cbf99a873b030eb957446
NN <no@ema.il>
2018-01-22 21:46:41 +0100

    Start of next page, with hand over of Survey id. Nothing more yet.

---
+++ survey-ui/src/main/java/com/cgi/bootcamp/survey/ui/models/SurveyFieldsAttributes.java
@@ -0,0 +1,22 @@
+package com.cgi.bootcamp.survey.ui.models;
+
+public class SurveyFieldsAttributes {
+
+	private String title;
+	
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+
+	@Override
+	public String toString() {
+		return "SurveyFieldsAttributes [title=" + title + "]";
+	}
+
+	
+}
